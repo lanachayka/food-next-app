74ec537b5e7524b239151da888537e6cc38542f9
RuslanaPidsadiuk-brickredsystems <ruslana.pidsadiuk@brickredsystems.ca>
2025-05-24 11:29:59 +0300

    Configure GitHub Actions for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/food-next-app' : '',
-  assetPrefix: isProd ? '/food-next-app' : '',
+  basePath: '/food-next-app',
+  assetPrefix: '/food-next-app',
 };
 
 export default nextConfig;
